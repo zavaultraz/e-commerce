68ad9a6bab12dc0d1e855bacc938141645d07d40
zavaultraz <zavazaneta@gmail.com>
2024-05-15 15:05:06 +0700

    up

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Models\Category;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class FrontendController extends Controller
 {
-    public function index(){
-        return view('pages.frontend.index');
+    public function index()
+    {
+
+        $category = Category::select('id', 'name')->latest()->get();
+        $product = Product::with('product_galleries')->select('id', 'name', 'slug', 'price')->latest()->get();
+
+        // dd($product);
+
+        return view('pages.frontend.index2',compact('category','product'));
     }
 }
