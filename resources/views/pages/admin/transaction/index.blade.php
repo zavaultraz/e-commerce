e5614c00a420052310445fb5be9383a20c7dab61
zavaultraz <zavazaneta@gmail.com>
2024-05-22 15:01:07 +0700

    user transaction

---
+++ resources/views/pages/admin/transaction/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.parent')
+@section('title', 'My Transaction')
+@section('content')
+
+<div class="card">
+    <div class="card-body">
+        <div>
+            <h1 class="fs-1 mt-3 card-title">My Transaction</h1>
+            <nav class="">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="#">Transaction</a></li>
+                    <li class="breadcrumb-item active">Transaction</li>
+                </ol>
+            </nav>
+        </div>
+        <h3 class="card-title">
+            <i class="bi bi-cart"></i> List Transaction
+        </h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Account</th>
+                    <th scope="col">Reciever Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($transaction as $row)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ auth()->user()->name }}</td>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->email }}</td>
+                    <td>{{ $row->phone }}</td>
+                    <td>
+                        @if ($row->status == 'EXPIRED')
+                        <span class="badge bg-danger">Expired</span>
+                        @elseif ($row->status == 'PENDING')
+                        <span class="badge bg-warning">Pending</span>
+                        @elseif ($row->status == 'SETTLEMENT')
+                        <span class="badge bg-info">Settelment</span>
+                        @else
+                        <span class="badge bg-succsess">Success</span>
+                        @endif
+                    </td>
+                    <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#viewModal{{$row->id}}">
+                            View
+                        </button></td>
+                    @include('pages.admin.my-transaction.modal.view-modal')
+
+                </tr>
+                @empty
+
+                @endforelse
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
